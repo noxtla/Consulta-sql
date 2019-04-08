289929adcd866d5d5f5a268060d6f293fd18e1c6
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-04-08 13:36:08 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
+	private void botonConectarActionPerformed(java.awt.event.ActionEvent evt) {                                              
+		Connection laConexion = getConnection();
+		PreparedStatement ps;
+		ResultSet rs;
 
+		try {
+			ps = laConexion.prepareStatement("select * from personas");
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				JOptionPane.showMessageDialog(null, "nombre" +rs.getString("nombre"));
+				///String.valueOf(rs.getDate("fecha_nacimiento"));   
+			}
+			else {
+				///No hay registros
+			}
+			laConexion.close();
+
+		} catch (Exception e) {
+			// Todo se derrumbo
+		}
+
+	}
